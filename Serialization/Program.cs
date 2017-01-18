ee6b28eaa583cd72f9a2f23db38932745f257fba
musoftware <MUGenius@windowslive.com>
2017-01-18 17:26:10 +0200

    first

---
+++ Serialization/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Serialization
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SerializationClass cc = new SerializationClass();
+
+            cc.AddProcess(() =>
+            {
+                Console.WriteLine("1 Action");
+                Thread.Sleep(1000);
+            });
+            cc.Run();
+            cc.AddProcess(() =>
+            {
+                Console.WriteLine("2 Action");
+                Thread.Sleep(1000);
+            });
+            cc.Run();
+            cc.AddProcess(() =>
+            {
+                Console.WriteLine("3 Action");
+                Thread.Sleep(1000);
+            });
+
+            cc.Run();
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Serialization/SerializationClass.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading;
+
+namespace Serialization
+{
+    class SerializationClass
+    {
+        Thread mainThread;
+
+        Collection<Action> actions = new Collection<Action>();
+
+        public void AddProcess(Action action)
+        {
+            actions.Add(action);
+        }
+
+        public void Run()
+        {
+            if (mainThread != null) return;
+            mainThread = new Thread(() =>
+            {
+                try
+                {
+                    while (true)
+                    {
+                        if (actions.Count == 0) break;
+                        if (actions[0] == null)
+                        {
+                            actions.RemoveAt(0);
+                        }
+                        if (actions.Count == 0) break;
+                        actions[0].Invoke();
+                        actions.RemoveAt(0);
+                    }
+                }
+                catch
+                {
+
+                }
+                finally
+                {
+                    mainThread = null;
+                }
+            });
+            mainThread.Start();
+        }
+
+
+    }
+}
